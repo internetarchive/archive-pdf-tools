f2671da2452eb1b86503c8c520ece87bf2c2c774
Merlijn Wajer <merlijn@wizzup.org>
2020-11-13 01:22:39 +0100

    Add more tools/

---
+++ run-all-tools.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+img=$1
+prefix=$2
+
+time python tools/threshold-image --with-invert-image --with-pregaussblur $img /tmp/$2-mask.png
+time python tools/threshold-image --with-invert-image --with-pregaussblur $img /tmp/$2-maski.png --invert-mask
+
+tools/partial-blur $img /tmp/$2-maski.png /tmp/$2-bg.png
+tools/partial-blur $img /tmp/$2-mask.png /tmp/$2-fg.png
+
+tools/compress /tmp/$2-mask.png /tmp/$2-fg.png /tmp/$2-bg.png /tmp/$2-fg.jp2 /tmp/$2-bg.jp2
+
+
+tools/merge /tmp/$2-mask.png /tmp/$2-fg.jp2 /tmp/$2-bg.jp2 /tmp/$2-z-combined.png
+#tools/merge /tmp/$2-mask.png /tmp/$2-fg.png /tmp/$2-bg.png /tmp/$2-z-combined.png
