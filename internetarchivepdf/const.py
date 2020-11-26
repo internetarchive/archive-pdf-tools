a0ceca8f93a847050a036ba111d214d6e54d0a74
Merlijn Wajer <merlijn@wizzup.org>
2020-11-26 13:59:50 +0100

    Make recode a library call
    This should make it easier to pass argument metadata like '--', since
    argparse has no sensible way to pass that as value (which makes sense,
    to be honest).
    
    Later on I will do some more reworking of internetarchivepdf/recode.py
    to make it more library-esque and less like a call to a
    executable/binary (clean up).

---
+++ internetarchivepdf/const.py
@@ -0,0 +1,11 @@
+VERSION = '0.0.1'
+
+SOFTWARE_URL = 'https://git.archive.org/merlijn/archive-pdf-tools'
+PRODUCER = 'Internet Archive PDF creator and recoder %s; %s; written by '\
+           'Merlijn B. W. Wajer. Powered by Tesseract, mupdf and '\
+           'Python (pymupdf/skimage).' % (VERSION, SOFTWARE_URL)
+
+
+IMAGE_MODE_PASSTHROUGH = 0
+IMAGE_MODE_PIXMAP = 1
+IMAGE_MODE_MRC = 2
