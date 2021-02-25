75b7e7a527eff73e0f302d5652523e1612e3492d
Merlijn Wajer <merlijn@wizzup.org>
2021-02-25 14:40:45 +0100

    setup: read version from const

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 from setuptools import setup
+from distutils.util import convert_path
+
+main_ns = {}
+ver_path = convert_path('internetarchivepdf/const.py')
+with open(ver_path) as ver_file:
+    exec(ver_file.read(), main_ns)
 
 setup(name='internetarchivepdf',
-      version='0.0.1',
+      version=main_ns['__version__'],
       description='Internet Archive PDF compression tools',
       author='Merlijn Boris Wolf Wajer',
       author_email='merlijn@archive.org',
