508c0c4d5b621b17c0c34707821585a2544072e3
Merlijn Wajer <merlijn@wizzup.org>
2021-11-28 18:22:07 +0100

    version 1.4.10 with new hocr

--- internetarchivepdf/const.py
+++ internetarchivepdf/const.py
@@ -16,7 +16,7 @@
 #
 # Author: Merlijn Boris Wolf Wajer <merlijn@archive.org>
 
-VERSION = '1.4.9'
+VERSION = '1.4.10'
 
 PRODUCER = 'Internet Archive PDF %s; including '\
            'mupdf and pymupdf/skimage' % (VERSION,)
