84f38e322d411930e417194cd7ff0f73f18e1afb
geisserml <geisserml@gmail.com>
2022-01-08 23:56:52 +0100

    simplify accessing version
    The `attr` directive does not import the file, so no issues if
    dependencies aren't installed yet.
    I'm not sure how to construct the download url in setup.cfg, though...

--- setup.py
+++ setup.py
@@ -8,15 +8,7 @@ import numpy
 import os
 os.environ['CFLAGS'] = '-Ofast -DNPY_NO_DEPRECATED_API'
 
-main_ns = {}
-ver_path = convert_path('internetarchivepdf/const.py')
-with open(ver_path) as ver_file:
-    exec(ver_file.read(), main_ns)
-
-version = main_ns['__version__']
 setup(
-    version = version,
-    download_url = 'https://github.com/internetarchive/archive-pdf-tools/archive/%s.tar.gz' % version,
     include_dirs = [numpy.get_include()],
     ext_modules = cythonize(
         ['cython/sauvola.pyx', 'cython/optimiser.pyx'],
