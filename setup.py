7268cad9c6fc378d26a91346c3e44b7c2e43e201
geisserml <geisserml@gmail.com>
2022-01-20 15:18:08 +0100

    Fix setup by reading the version file manually
    I thought that `attr:` does extract the attribute literally, but
    apparently it tries to import the file anyway.

--- setup.py
+++ setup.py
@@ -1,17 +1,25 @@
-from setuptools import setup
-from distutils.util import convert_path
-
-from Cython.Build import cythonize
 import numpy
+from setuptools import setup, convert_path
+from Cython.Build import cythonize
 
 # Work around bugs in passing CFLAGS to the Cython compilation process
 import os
 os.environ['CFLAGS'] = '-Ofast -DNPY_NO_DEPRECATED_API'
 
-setup(
-    include_dirs = [numpy.get_include()],
-    ext_modules = cythonize(
-        ['cython/sauvola.pyx', 'cython/optimiser.pyx'],
-        compiler_directives = {'language_level' : '3'},
-    ),
-)
+if __name__ == '__main__':
+
+    main_ns = {}
+    ver_path = convert_path('internetarchivepdf/const.py')
+    with open(ver_path) as ver_file:
+        exec(ver_file.read(), main_ns)
+
+    version = main_ns['__version__']
+    setup(
+        version = version,
+        download_url='https://github.com/internetarchive/archive-pdf-tools/archive/%s.tar.gz' % version,
+        include_dirs = [numpy.get_include()],
+        ext_modules = cythonize(
+            ['cython/sauvola.pyx', 'cython/optimiser.pyx'],
+            compiler_directives = {'language_level' : '3'},
+        ),
+    )
