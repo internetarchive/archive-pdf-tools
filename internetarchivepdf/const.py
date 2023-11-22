4d72c5123e8d1da29ae459299bbce7f3761743c0
Merlijn Wajer <merlijn@wizzup.org>
2023-11-22 22:01:26 +0100

    version 1.4.23

--- internetarchivepdf/const.py
+++ internetarchivepdf/const.py
@@ -16,7 +16,7 @@
 #
 # Author: Merlijn Boris Wolf Wajer <merlijn@archive.org>
 
-VERSION = '1.4.22'
+VERSION = '1.4.23'
 
 PRODUCER = 'Internet Archive PDF %s; including '\
            'mupdf and pymupdf/skimage' % (VERSION,)
